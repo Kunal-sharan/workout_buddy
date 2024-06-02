9239903a4c05059c4aa94005d2e7f99f48cd90a5
Kunal sharan <117339459+Kunal-sharan@users.noreply.github.com>
2024-06-02 14:17:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from audiorecorder import audiorecorder
-import speech_recognition as sr
+# from audiorecorder import audiorecorder
+# import speech_recognition as sr
 import os
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.prompts import PromptTemplate
@@ -8,6 +8,8 @@ from langchain_google_genai import GoogleGenerativeAI
 import pandas as pd
 import time
 import ast
+from streamlit_mic_recorder import speech_to_text
+
 llm = GoogleGenerativeAI(model="gemini-pro", google_api_key=st.secrets['GOOGLE_API'])
 
 parser = JsonOutputParser()
@@ -21,25 +23,35 @@ prompt = PromptTemplate(
 chain = prompt | llm
 
 # res=chain.invoke({"query": gym_query})
-r = sr.Recognizer()
+# r = sr.Recognizer()
 
 if "workouts" not in st.session_state:
   st.session_state.workouts=[]
 text=""
 st.set_page_config(layout="wide")
 st.title("Workout Tracker")
-audio = audiorecorder("", "")
-if len(audio) > 0:
+text = speech_to_text(
+    language='en',
+    start_prompt="Start recording",
+    stop_prompt="Stop recording",
+    just_once=False,
+    use_container_width=False,
+    callback=None,
+    args=(),
+    kwargs={},
+    key=None
+)
+if len(text) > 0:
     # # To play audio in frontend:
     # st.audio(audio.export().read())
     # To save audio to a file, use pydub export method:
-    audio.export("audio_input.wav", format="wav")
+    # audio.export("audio_input.wav", format="wav")
     
-    # To get audio properties, use pydub AudioSegment properties:
-    # st.write(f"Frame rate: {audio.frame_rate}, Frame width: {audio.frame_width}, Duration: {audio.duration_seconds} seconds")
-    with sr.AudioFile("audio_input.wav") as source:
-      a=r.record(source)
-      text=r.recognize_google(a)
+    # # To get audio properties, use pydub AudioSegment properties:
+    # # st.write(f"Frame rate: {audio.frame_rate}, Frame width: {audio.frame_width}, Duration: {audio.duration_seconds} seconds")
+    # with sr.AudioFile("audio_input.wav") as source:
+    #   a=r.record(source)
+    #   text=r.recognize_google(a)
     # if len(text)>0:
     #   text=llm.invoke(f"The given text is related to gym analyze the complete text and fix it if random or wrong words are written make it proper don't add any extra information {text} your ouput should be a one line sentence that only containes these things  'Target Muscle','Workout','Reps per set','Sets' and make sure the 'Sets'  is not more than 4 ")
     st.write(text)
